ab514dd62f33cdef9a22744419c675423e144ef2
MaudOtten <maudottenheijm@gmail.com>
2021-06-08 08:33:44 +0200

    Start on fmriprep

--- scripts/frmiprep.py
+++ scripts/frmiprep.py
@@ -1,2 +1,12 @@
-import fmriprep
+# run bash install for freesurfer first (from $HOME on wsl Ubuntu-20.04)
+# export FREESURFER_HOME=$HOME/freesurfer
+# export SUBJECTS_DIR=$FREESURFER_HOME/subjects
+# source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+import subprocess
+
+check_fs = subprocess.run("which freeview", capture_output=True, shell=True)
+assert check_fs.returncode == 0
+
+
 
