7d16a1ab762599c76c95eebf40bfffc0f37ab30b
MaudOtten <maudottenheijm@gmail.com>
2021-06-08 08:34:24 +0200

    Start on coregistration

--- scripts/full_analysis.m
+++ scripts/full_analysis.m
@@ -2,13 +2,14 @@
 #run install_spm.m
 
 # Run VDM calc on each session
-data_dir = "C:/Users/maudo/Documents/Projects/OpenNeuro/test_data/";
+sdata.root_dir = "C:/Users/maudo/Documents/Projects/OpenNeuro/"
+sdata.data_dir = [sdata.root_dir, "ds-spm/"];
 sdata.sub = "sub-01/";
 
 
 for i = 1:5
   sdata.ses = sprintf("ses-%02d", i);
-  sdata.ses_dir = [data_dir, sdata.sub, sdata.ses];
+  sdata.ses_dir = [sdata.data_dir, sdata.sub, sdata.ses];
   
   matlabbatch = load_batch(1, sdata);
   save(["calc_VDM_", sdata.ses ,".mat"], "matlabbatch");
